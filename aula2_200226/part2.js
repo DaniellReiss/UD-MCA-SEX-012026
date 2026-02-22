a41eb04990163496298b633f40f3524c881f6c23
Igor Euripedes <igoreuripedes9@gmail.com>
2026-02-22 14:17:53 -0300

    Implement object examples and console logging in part2.js

---
+++ aula2_200226/part2.js
@@ -0,0 +1,81 @@
+//Vitor Bispo Borges
+//RA: 825242612
+// part2.js
+console.log("--- Respostas da Parte 2 ---");
+
+// Criando o objeto com as propriedades que o professor pediu
+let meuCarroDosSonhos = {
+  nome: 'McLaren F1',
+  cor: 'Prata',
+  preco: 100000000, // Cem milhões, sonhar é de graça rs
+  estoque: 2
+};
+
+// A. Como acessar o nome do objeto?
+console.log("A) Nome do objeto:", meuCarroDosSonhos.nome);
+
+// B. Como acessar o preço usando colchetes?
+// Essa eu demorei a lembrar na aula, tem que por as aspas!
+console.log("B) Preço usando colchetes:", meuCarroDosSonhos['preco']);
+
+// C. Atualize o estoque para 80.
+meuCarroDosSonhos.estoque = 80;
+console.log("C) Novo estoque atualizado para:", meuCarroDosSonhos.estoque);
+
+// D. Imprima todas as propriedades no console.
+console.log("D) Imprimindo todas as propriedades no console:");
+console.log(meuCarroDosSonhos);
+
+//Thiago Viana Meira
+//RA: 82424566
+// part2.js
+console.log("Parte 2");
+
+// Criando o objeto com as propriedades que o professor pediu
+let especierara = {
+  nome: 'Death’s Head Cockroach',
+  cor: 'Preta com marrom claro',
+  preco: 20,
+  estoque: 167
+};
+
+// Como acessar o nome do objeto?
+console.log("A) Nome do objeto:", especierara.nome);
+
+// Como acessar o preço usando colchetes?
+console.log("B) Preço usando colchetes:", especierara['preco']);
+
+// Atualize o estoque para 80.
+especierara.estoque = 80;
+console.log("C) Novo estoque atualizado para:", especierara.estoque);
+
+// D. Imprima todas as propriedades no console.
+console.log("D) Imprimindo todas as propriedades no console:");
+console.log(especierara);
+
+//Igor Euripedes Garcia de Oliveira
+//RA: 824117373
+// part2.js
+console.log("--- Respostas da Parte 2 ---");
+
+// Criando o objeto com as propriedades do jogo
+let meuJogoDosSonhos = { 
+  nome: 'Monster Hunter: World',
+  genero: 'RPG de Ação',
+  preco: 249.90,
+  estoque: 2
+};
+
+// A. Como acessar o nome do objeto?
+console.log("A) Nome do jogo:", meuJogoDosSonhos.nome);
+
+// B. Como acessar o preço usando colchetes?
+console.log("B) Preço usando colchetes:", meuJogoDosSonhos['preco']);
+
+// C. Atualize o estoque para 80.
+meuJogoDosSonhos.estoque = 80;
+console.log("C) Novo estoque atualizado para:", meuJogoDosSonhos.estoque);
+
+// D. Imprima todas as propriedades no console.
+console.log("D) Imprimindo todas as propriedades no console:");
+console.log(meuJogoDosSonhos);
